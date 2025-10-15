4596dac2d2809cc6b278c4401d79360fd4a0314b
Stefan Gojan <vollekannehoschi@gmail.com>
2025-10-15 18:48:42 +0200

    hello world

--- apps/rest-server/src/index.ts
+++ apps/rest-server/src/index.ts
@@ -1,17 +1,3 @@
-import {
-  HttpApi,
-  HttpApiBuilder,
-  HttpApiEndpoint,
-  HttpApiGroup
-} from "@effect/platform"
-import { NodeHttpServer, NodeRuntime } from "@effect/platform-node"
-import { Effect, Layer, Schema } from "effect"
-import { createServer } from "node:http"
+import { hello } from "@repo/video-service/video"
 
-const ServerLive = HttpApiBuilder.serve().pipe(
-  Layer.provide(VideoApiLive),
-  Layer.provide(NodeHttpServer.layer(createServer, { port: 3055 }))
-)
-
-// Launch the server
-Layer.launch(ServerLive).pipe(NodeRuntime.runMain)
+console.log(hello('mytest'))
