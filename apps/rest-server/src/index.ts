b8d3f43c0da152545ea8973594a675c4e09f4355
Stefan Gojan <vollekannehoschi@gmail.com>
2025-10-16 10:45:01 +0200

    simple server

--- apps/rest-server/src/index.ts
+++ apps/rest-server/src/index.ts
@@ -1,3 +1,19 @@
-import { hello } from "@repo/video-service/video"
+import { hello, MyApiLive } from "@repo/video-service/video";
+import { Layer } from "effect";
+import { BunHttpServer, BunRuntime } from "@effect/platform-bun";
+import {
+    HttpApiBuilder
+} from "@effect/platform";
+import { createServer } from "node:http";
+
+
 
 console.log(hello('mytest'))
+
+const ServerLive = HttpApiBuilder.serve().pipe(
+    Layer.provide(MyApiLive),
+    Layer.provide(BunHttpServer.layer(createServer))
+)
+
+// Launch the server
+Layer.launch(ServerLive).pipe(BunRuntime.runMain)
--- packages/video-service/src/video.ts
+++ packages/video-service/src/video.ts
@@ -1,1 +1,24 @@
+import {
+    HttpApi,
+    HttpApiBuilder,
+    HttpApiEndpoint,
+    HttpApiGroup
+} from "@effect/platform"
+import { Effect, Layer, Schema } from "effect"
+
+// Define our API with one group named "Greetings" and one endpoint called "hello-world"
+const MyApi = HttpApi.make("MyApi").add(
+    HttpApiGroup.make("Greetings").add(
+        HttpApiEndpoint.get("hello-world")`/`.addSuccess(Schema.String)
+    )
+)
+
+// Implement the "Greetings" group
+const GreetingsLive = HttpApiBuilder.group(MyApi, "Greetings", (handlers) =>
+    handlers.handle("hello-world", () => Effect.succeed("Hello, World!!!"))
+)
+
+// Provide the implementation for the API
+export const MyApiLive = HttpApiBuilder.api(MyApi).pipe(Layer.provide(GreetingsLive))
+
 export const hello = (input: string): string => `hallo: ${input}`
