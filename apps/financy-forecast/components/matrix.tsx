a9145f35082ed48684735127d71484f7d8bd1f48
Stefan Gojan <vollekannehoschi@gmail.com>
2025-12-14 15:49:10 +0100

    get details

--- apps/financy-forecast/components/matrix.tsx
+++ apps/financy-forecast/components/matrix.tsx
@@ -1,15 +1,22 @@
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
-import { getAssetSnapshots } from "@/lib/db"
+import { getSnapshotDetails } from "@/lib/db"
+import { Option } from 'effect';
+import { isNone } from "effect/Option";
 
 export async function Matrix() {
-    const snapshots = await getAssetSnapshots()
+    const snapshots = await getSnapshotDetails(5)
+
+    if (isNone(snapshots)) {
+        return <div>no data</div>
+    }
+
     console.log(snapshots)
     return (
 
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead className="w-[100px]">Konten -- {snapshots.length}</TableHead>
+                    <TableHead className="w-[100px]">Konten -- </TableHead>
                     <TableHead>Status</TableHead>
                     <TableHead>Method</TableHead>
                     <TableHead className="text-right">Amount</TableHead>
