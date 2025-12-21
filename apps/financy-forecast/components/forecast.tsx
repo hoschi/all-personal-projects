b3d5a018ec19715f945801b4de4940ea2922c148
Stefan Gojan <vollekannehoschi@gmail.com>
2025-12-21 19:43:28 +0100

    I hate CSS

--- apps/financy-forecast/components/forecast.tsx
+++ apps/financy-forecast/components/forecast.tsx
@@ -16,7 +16,7 @@ export async function Forecast() {
     const forecastData = Option.getOrUndefined(forecastDataResult)
 
     return (
-        <>
+        <div className="flex flex-col h-dvh">
             {forecastData && <ForecastDataInitializer forecastData={forecastData} />}
             <header className="flex items-center gap-2 m-3 ml-8">
                 <SidebarTrigger className="-ml-1 mr-3" />
@@ -29,15 +29,37 @@ export async function Forecast() {
                     })}
                 </div>
             </header>
-            <div className="p-4">
-                {Option.match(forecastDataResult, {
-                    onNone: () => <div>no data</div>,
-                    onSome: (forecastData) => <Timeline forecastData={forecastData} />
-                })}
-            </div>
-        </>
+            {Option.match(forecastDataResult, {
+                onNone: () => <div>no data</div>,
+                onSome: (forecastData) => <Timeline forecastData={forecastData} />
+            })}
+        </div>
     )
 }
+
+
+/*
+export async function Forecast() {
+                'use cache'
+    cacheTag('snapshots')
+
+            const forecastDataResult = await getForecastData()
+            const forecastData = Option.getOrUndefined(forecastDataResult)
+
+            return (
+            <div className="bg-amber-300 flex flex-col p-4 overflow-hidden h-dvh overflow-x-auto overflow-y-hidden">
+                <header>
+                    header
+                </header>
+                <div className="bg-red-500 flex-1 flex flex-col p-4 flex-wrap h-full">
+                    {data.map((_, i) => (
+                        <div key={i} className="w-[340px] md:w-[360px] flex-shrink-0 relative rounded-sm border transition-all duration-200 group bg-slate-100 border-slate-200 opacity-90 hover:opacity-100 hover:border-slate-300 flex items-center h-[60px] px-4">test {i}</div>
+                    ))}
+                </div>
+            </div>
+            )
+}
+            */
 
 export function ForecastHeader({ forecastData }: { forecastData: ForecastTimelineData; }) {
     const startAmount = forecastData.startAmount
