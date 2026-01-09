ce4cf6ff0f67a04cdfc4e1015641608c7b35a1d2
Stefan Gojan <vollekannehoschi@gmail.com>
2026-01-09 18:59:49 +0100

    fix

--- apps/switch-test/eslint.config.js
+++ apps/switch-test/eslint.config.js
@@ -1,9 +1,10 @@
-import { tanstackConfig } from "@tanstack/eslint-config"
-
-//console.log(tanstackConfig)
+// import { tanstackConfig } from "@tanstack/eslint-config"
+import { config } from "@repo/eslint-config/base"
 
 export default [
+  // "eslint.config.js", TODO https://github.com/TanStack/create-tsrouter-app/issues/80
   //...tanstackConfig,
+  ...config,
   {
     ignores: [".output/"],
   },
