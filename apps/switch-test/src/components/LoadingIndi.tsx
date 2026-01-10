d7d920fe52ba88955fdd558efcd1361c4c40f50d
Stefan Gojan <vollekannehoschi@gmail.com>
2026-01-10 15:40:25 +0100

    loading indicator

---
+++ apps/switch-test/src/components/LoadingIndi.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from "./ui/spinner";
+
+export function LoadingIndi() {
+    return <Spinner className="size-6" />
+}
--- apps/switch-test/src/router.tsx
+++ apps/switch-test/src/router.tsx
@@ -2,12 +2,14 @@ import { createRouter } from "@tanstack/react-router"
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
+import { LoadingIndi } from "./components/LoadingIndi"
 
 // Create a new router instance
 export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: {},
+    defaultPendingComponent: LoadingIndi,
 
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
