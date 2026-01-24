ca5632802262cf08647e805a8b2be020b4c40223
Stefan Gojan <vollekannehoschi@gmail.com>
2026-01-24 17:58:29 +0100

    jesus, next try

--- apps/box-storage/src/data/prisma.ts
+++ apps/box-storage/src/data/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 import { config, DATABASE_SCHEMA_NAME } from "prisma.config"
